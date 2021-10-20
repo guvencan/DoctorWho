6eecc0b58d46bdb87cf3cdd85bba87b7468d53dc
karahantaylan <54514299+karahantaylan@users.noreply.github.com>
2021-10-20 04:42:04 +0300

    firebase enabled

--- DoctorWho/Profile/ProfileViewController.swift
+++ DoctorWho/Profile/ProfileViewController.swift
@@ -6,13 +6,25 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ProfileViewController: UIViewController{
     
+    var ref: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Profile"
+        
+        let ref = Database.database(url: "https://doctorwho-bb2c7-default-rtdb.europe-west1.firebasedatabase.app").reference()
+        ref.child("asd").setValue("ttttttt")
+        
+        ref.child("asd").observeSingleEvent(of: .value, with: { (snapshot) in
+        if let tit = snapshot.value as? String {
+        print("The value from the database: \(tit)")
+            self.title = tit
+        }
+        })
     }
     
     
