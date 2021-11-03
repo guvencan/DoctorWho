572f6193236e90fd38da49cdad0fafc6a59dba50
Güvencan Karpat <guvencan.karpat@ing.com.tr>
2021-11-03 22:43:57 +0300

    resolved merge conflict

--- DoctorWho/Common/Util.swift
+++ DoctorWho/Common/Util.swift
@@ -9,19 +9,6 @@ import Foundation
 import UIKit
 
 
-func readLocalFile(forName name: String) -> Data? {
-    do {
-        if let bundlePath = Bundle.main.path(forResource: name, ofType: "json"),
-            let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
-            return jsonData
-        }
-    } catch {
-        print(error)
-    }
-    
-    return nil
-}
-
 extension UIColor {
     convenience init(hexString : String) {
         if let rgbValue = UInt(hexString, radix: 16) {
